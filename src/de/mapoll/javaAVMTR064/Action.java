90114cdd39e7aeaa387b1b7e2e93ca96ff08aa91
Inguiotar <j.gretza@anetos.de>
2017-02-23 01:10:48 +0100

    Add files via upload

--- src/de/mapoll/javaAVMTR064/Action.java
+++ src/de/mapoll/javaAVMTR064/Action.java
@@ -52,201 +52,204 @@ import de.mapoll.javaAVMTR064.beans.StateVariableType;
 
 public class Action {
 
-	private Map<String, Type> stateToType;
-	private Map<String, Boolean> argumentOut;
-	private Map<String, String> argumentState;
-	private String name;
-	private ActionType actionXML;
-	private FritzConnection connection;
-	private ServiceType serviceXML;
+    private Map<String, Type> stateToType;
+    private Map<String, Boolean> argumentOut;
+    private Map<String, String> argumentState;
+    private String name;
+    private ActionType actionXML;
+    private FritzConnection connection;
+    private ServiceType serviceXML;
 
-	public Action(ActionType action, List<StateVariableType> stateVariableList, FritzConnection connection,
-			ServiceType serviceXML) {
-		this.actionXML = action;
-		stateToType = new HashMap<String, Type>();
-		argumentOut = new HashMap<String, Boolean>();
-		argumentState = new HashMap<String, String>();
-		name = actionXML.getName();
-		this.connection = connection;
-		this.serviceXML = serviceXML;
-		for (StateVariableType s : stateVariableList) {
-			Type type = null;
-			if (s.getDataType().equals("string"))
-				type = String.class;
-			else if (s.getDataType().startsWith("ui") || s.getDataType().startsWith("i"))
-				type = Integer.class;
-			else if (s.getDataType().equals("boolean"))
-				type = Boolean.class;
-			else if (s.getDataType().equals("dateTime"))
-				type = Date.class;
-			else if (s.getDataType().equals("uuid"))
-				type = UUID.class;
-			else
-				System.err.println("UNKNOWNE TYPE:" + s.getDataType() + " " + s.getName() + "  " + actionXML.getName());
-			stateToType.put(s.getName(), type);
-		}
-		if (actionXML.getArgumentList() != null)
-			for (ArgumentType a : actionXML.getArgumentList().getArgument()) {
-				String name = a.getName();
-				argumentOut.put(name, a.getDirection().equals("out"));
-				argumentState.put(name, a.getRelatedStateVariable());
-			}
-	}
+    public Action(ActionType action, List<StateVariableType> stateVariableList, FritzConnection connection,
+            ServiceType serviceXML) {
+        this.actionXML = action;
+        stateToType = new HashMap<String, Type>();
+        argumentOut = new HashMap<String, Boolean>();
+        argumentState = new HashMap<String, String>();
+        name = actionXML.getName();
+        this.connection = connection;
+        this.serviceXML = serviceXML;
+        for (StateVariableType s : stateVariableList) {
+            Type type = null;
+            if (s.getDataType().equals("string")) {
+                type = String.class;
+            } else if (s.getDataType().startsWith("ui") || s.getDataType().startsWith("i")) {
+                type = Integer.class;
+            } else if (s.getDataType().equals("boolean")) {
+                type = Boolean.class;
+            } else if (s.getDataType().equals("dateTime")) {
+                type = Date.class;
+            } else if (s.getDataType().equals("uuid")) {
+                type = UUID.class;
+            } else {
+                System.err.println("UNKNOWNE TYPE:" + s.getDataType() + " " + s.getName() + "  " + actionXML.getName());
+            }
+            stateToType.put(s.getName(), type);
+        }
+        if (actionXML.getArgumentList() != null) {
+            for (ArgumentType a : actionXML.getArgumentList().getArgument()) {
+                String name = a.getName();
+                argumentOut.put(name, a.getDirection().equals("out"));
+                argumentState.put(name, a.getRelatedStateVariable());
+            }
+        }
+    }
 
-	public String toString() {
-		String ret = getName() + ":";
-		for (String argument : argumentOut.keySet()) {
-			ret += " " + argument + (argumentOut.get(argument) ? " out" : " in") + " "
-					+ stateToType.get(argumentState.get(argument)).getTypeName();
-		}
+    public String toString() {
+        String ret = getName() + ":";
+        for (String argument : argumentOut.keySet()) {
+            ret += " " + argument + (argumentOut.get(argument) ? " out" : " in") + " "
+                    + stateToType.get(argumentState.get(argument)).getTypeName();
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Set<String> getArguments() {
-		return argumentOut.keySet();
-	}
+    public Set<String> getArguments() {
+        return argumentOut.keySet();
+    }
 
-	public boolean isOut(String argument) throws UnsupportedOperationException {
-		if (!argumentOut.containsKey(argument))
-			throw new UnsupportedOperationException(argument);
-		return argumentOut.get(argument);
-	}
+    public boolean isOut(String argument) throws UnsupportedOperationException {
+        if (!argumentOut.containsKey(argument)) {
+            throw new UnsupportedOperationException(argument);
+        }
+        return argumentOut.get(argument);
+    }
 
-	public boolean isIn(String argument) throws UnsupportedOperationException {
-		return !isOut(argument);
-	}
+    public boolean isIn(String argument) throws UnsupportedOperationException {
+        return !isOut(argument);
+    }
 
-	public Type getTypeOfArgument(String argument) throws UnsupportedOperationException {
-		if (!argumentOut.containsKey(argument))
-			throw new UnsupportedOperationException(argument);
-		return stateToType.get(argumentState.get(argument));
-	}
+    public Type getTypeOfArgument(String argument) throws UnsupportedOperationException {
+        if (!argumentOut.containsKey(argument)) {
+            throw new UnsupportedOperationException(argument);
+        }
+        return stateToType.get(argumentState.get(argument));
+    }
 
-	public Response execute() throws UnsupportedOperationException, IOException {
-		return this.execute(null);
-	}
+    public Response execute() throws UnsupportedOperationException, IOException {
+        return this.execute(null);
+    }
 
-	public Response execute(Map<String, Object> arguments) throws UnsupportedOperationException, IOException {
-		Set<String> inArguments = new HashSet<String>();
-		for (String a : argumentOut.keySet())
-			if (!argumentOut.get(a))
-				inArguments.add(a);
-		if (arguments != null) {
-			for (String a : arguments.keySet()) {
-				Object parameter = arguments.get(a);
-				if (!argumentOut.containsKey(a))
-					throw new UnsupportedOperationException(a + " is not an Argument for " + this.name);
-				if (argumentOut.get(a))
-					throw new UnsupportedOperationException(a + " is not In");
-				if (parameter.getClass() != this.getTypeOfArgument(a)) {
-					try {
-						parameter = Class.forName(this.getTypeOfArgument(a).getTypeName()).cast(parameter);
-					} catch (Exception e) {
-						throw new UnsupportedOperationException(a + " has to be " + this.getTypeOfArgument(a)
-								+ ". Cast not posible: " + e.getMessage());
-					}
+    public Response execute(Map<String, Object> arguments) throws UnsupportedOperationException, IOException {
+        Set<String> inArguments = new HashSet<String>();
+        for (String a : argumentOut.keySet()) {
+            if (!argumentOut.get(a)) {
+                inArguments.add(a);
+            }
+        }
+        if (arguments != null) {
+            for (String a : arguments.keySet()) {
+                Object parameter = arguments.get(a);
+                if (!argumentOut.containsKey(a)) {
+                    throw new UnsupportedOperationException(a + " is not an Argument for " + this.name);
+                }
+                if (argumentOut.get(a)) {
+                    throw new UnsupportedOperationException(a + " is not In");
+                }
+                if (parameter.getClass() != this.getTypeOfArgument(a)) {
+                    try {
+                        parameter = Class.forName(this.getTypeOfArgument(a).getTypeName()).cast(parameter);
+                    } catch (Exception e) {
+                        throw new UnsupportedOperationException(a + " has to be " + this.getTypeOfArgument(a)
+                                + ". Cast not posible: " + e.getMessage());
+                    }
 
-				}
-				inArguments.remove(a);
-			}
+                }
+                inArguments.remove(a);
+            }
 
-		}
-		if (inArguments.size() > 0)
-			throw new UnsupportedOperationException("Missing In-Arguments: " + inArguments);
-		HttpEntity httpEntity = null;
-		String message = null;
-		try {
-			MessageFactory factory = MessageFactory.newInstance();
-			SOAPMessage soapMsg = factory.createMessage();
-			SOAPPart part = soapMsg.getSOAPPart();
-			SOAPEnvelope envelope = part.getEnvelope();
-			SOAPBody body = envelope.getBody();
-			envelope.setEncodingStyle("http://schemas.xmlsoap.org/soap/encoding/");
-			SOAPBodyElement element = body.addBodyElement(envelope.createName(this.name, "u",
-					serviceXML.getServiceType()));
-			if (arguments != null)
-			for (String key : arguments.keySet()) {
-				element.addChildElement(key).addTextNode(valueToSOAPString(key, arguments.get(key)));
-			}
-			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-			soapMsg.writeTo(stream);
-			message = new String(stream.toByteArray(), "utf-8");
-		} catch (SOAPException e) {
-			e.printStackTrace();
-		}
+        }
+        if (inArguments.size() > 0) {
+            throw new UnsupportedOperationException("Missing In-Arguments: " + inArguments);
+        }
+        HttpEntity httpEntity = null;
+        String message = null;
+        try {
+            MessageFactory factory = MessageFactory.newInstance();
+            SOAPMessage soapMsg = factory.createMessage();
+            SOAPPart part = soapMsg.getSOAPPart();
+            SOAPEnvelope envelope = part.getEnvelope();
+            SOAPBody body = envelope.getBody();
+            envelope.setEncodingStyle("http://schemas.xmlsoap.org/soap/encoding/");
+            SOAPBodyElement element = body.addBodyElement(envelope.createName(this.name, "u",
+                    serviceXML.getServiceType()));
+            if (arguments != null) {
+                for (String key : arguments.keySet()) {
+                    element.addChildElement(key).addTextNode(valueToSOAPString(key, arguments.get(key)));
+                }
+            }
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            soapMsg.writeTo(stream);
+            message = new String(stream.toByteArray(), "utf-8");
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
 
-		httpEntity = new StringEntity(message);
+        httpEntity = new StringEntity(message);
 
-		return getMessage(connection.getSOAPXMLIS(serviceXML.getControlURL(),
-				serviceXML.getServiceType() + "#" + this.getName(), httpEntity));
-	}
+        return getMessage(connection.getSOAPXMLIS(serviceXML.getControlURL(),
+                serviceXML.getServiceType() + "#" + this.getName(), httpEntity));
+    }
 
-	private String valueToSOAPString(String name, Object value) {
-		Class<?> classOfValue = null;
-		String ret="";
-		try {
-			classOfValue = Class.forName(this.getTypeOfArgument(name).getTypeName());
-		} catch (ClassNotFoundException | UnsupportedOperationException e) {
-			// Cant happen already checked
-			e.printStackTrace();
-		}
+    private String valueToSOAPString(String name, Object value) {
+        Class<?> classOfValue = null;
+        String ret = "";
+        try {
+            classOfValue = Class.forName(this.getTypeOfArgument(name).getTypeName());
+        } catch (ClassNotFoundException | UnsupportedOperationException e) {
+            // Cant happen already checked
+            e.printStackTrace();
+        }
 
-		if (classOfValue == String.class) {
-			ret =  (String) value;
-		}
+        if (classOfValue == String.class) {
+            ret = (String) value;
+        } else if (classOfValue == Integer.class) {
+            int tmp = (Integer) value;
+            ret = "" + tmp;
 
-		else if (classOfValue == Integer.class) {
-			int tmp = (Integer) value;
-			ret  = "" + tmp;
+        } else if (classOfValue == Boolean.class) {
+            boolean tmp = (Boolean) value;
+            ret = tmp ? "1" : "0";
 
-		}
+        } else if (classOfValue == Date.class) {
+            Date tmp = (Date) value;
+            String dateFormat = "yyyy-MM-dd'T'hh:mm:ss'Z'";
+            DateFormat lDateFormat = new SimpleDateFormat(dateFormat);
+            ret = lDateFormat.format(tmp);
+        } else if (classOfValue == UUID.class) {
+            UUID tmp = (UUID) value;
+            ret = tmp.toString();
+        }
 
-		else if (classOfValue == Boolean.class) {
-			boolean tmp = (Boolean) value;
-			ret = tmp ? "1" : "0";
+        return ret;
+    }
 
-		}
+    private Response getMessage(InputStream soapxmlis) throws IOException {
+        SOAPMessage response = null;
+        Response ret = null;
 
-		else if (classOfValue == Date.class) {
-			Date tmp = (Date) value;
-			String dateFormat = "yyyy-MM-dd'T'hh:mm:ss'Z'";
-			DateFormat lDateFormat = new SimpleDateFormat(dateFormat);
-			ret = lDateFormat.format(tmp);
-		}
+        try {
+            response = MessageFactory.newInstance().createMessage(null, soapxmlis);
+        } catch (IOException | SOAPException e) {
+            throw new IOException(e.getMessage());
+        }
 
-		else if (classOfValue == UUID.class) {
-			UUID tmp = (UUID) value;
-			ret = tmp.toString();
-		}
+        if (response == null) {
+            return null;
+        }
 
-		return ret;
-	}
+        try {
+            ret = new Response(response, stateToType, argumentState);
+        } catch (SOAPException e) {
+            throw new IOException(e.getMessage());
+        }
+        return ret;
 
-	private Response getMessage(InputStream soapxmlis) throws IOException {
-		SOAPMessage response = null;
-		Response ret = null;
-
-		try {
-			response = MessageFactory.newInstance().createMessage(null, soapxmlis);
-		} catch (IOException | SOAPException e) {
-			throw new IOException(e.getMessage());
-		}
-
-		if (response == null)
-			return null;
-
-		try {
-			ret = new Response(response, stateToType, argumentState);
-		} catch (SOAPException e) {
-			throw new IOException(e.getMessage());
-		}
-		return ret;
-
-	}
+    }
 
 }
